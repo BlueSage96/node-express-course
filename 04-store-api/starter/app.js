be0795b4d0f3890c6c5f4ae2c07dd35976e5b97c
Brittany Price <bprice0496@gmail.com>
2025-08-24 17:15:11 -0400

    Answers for lesson 7

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,38 @@
-console.log('04 Store API')
+process.noDeprecation = true;//suppress deprecation warnings in console
+require("dotenv").config();
+require('express-async-errors');
+
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const productsRouter = require('./routes/products');
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API </h1><a href="/api/v1/products">Products Route</a>');
+});
+
+// products route
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server's listening to port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
